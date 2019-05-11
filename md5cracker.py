e4b45cdf7991c853d846f708bf2da283f945e922
CodedChops <devham01@gmail.com>
2019-05-11 20:31:36 +0100

    Concise

--- md5cracker.py
+++ md5cracker.py
@@ -1,4 +1,4 @@
-import hashlib
+import hashlib,time
 
 md5_hash  = input("Please enter md5 hash: ")
 pass_file = input("Please add password file: ")
@@ -6,21 +6,20 @@ pass_file = input("Please add password file: ")
 counter = 0
 
 try:
-	file = open(pass_file, "r", encoding="utf8")
+        file = open(pass_file, "r", encoding="utf8", errors="ignore")
 except:
-	print("\nFile \" %s \" Not Found" % pass_file)
-	exit()
+        print("\nFile \" %s \" Not Found" % pass_file)
+        exit()
 
 for password in file:
-	
-	counter += 1
-	hashed_pass = hashlib.md5(password.encode()).hexdigest()
-	print("\rTrying Password #{0}: {1}".format(counter, password), end="")
-	
-	if hashed_pass == md5_hash:
-		print("\nPassword Found: %s" % password)
-		input()
-		break
 
+        counter += 1
+        hashed_pass = hashlib.md5(password.encode()).hexdigest()
+        print("Trying Password: %s" % counter, end="\r")
+
+        if hashed_pass == md5_hash:
+                print("\nPassword Found: %s" % password)
+                input()
+                break
 else:
-	print("\nPassword Not Found!")
+        print("\nPassword Not Found!")
